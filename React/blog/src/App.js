caa95b6a01a70e6c89118d70f8cf9c49e76da8d7
MinSungJe <minje813@naver.com>
2024-03-19 21:35:12 +0900

    20240319

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,16 +1,32 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '민성제식 혼밥 맛집';
+  let [title, setTitle] = useState(['가츠동 맛집 추천', '국밥 맛집 추천', '제육볶음 맛집 추천']);
+  let [like, setLike] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color: 'red', fontSize: '16px'}}>블로그 제목 들어갈거임</h4>
+        <h4 style={{color: 'red', fontSize: '16px'}}>React 쓰는 민성제</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{title[0]} <span onClick={()=>{setLike(like+1)}}>🥰</span> {like} </h4>
+        <p>3월 19일 발행</p> <button onClick={()=>{setTitle(['에비동 맛집 추천', '국밥 맛집 추천', '제육볶음 맛집 추천'])}}>다음</button>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>3월 20일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>3월 21일 발행</p>
+      </div>
     </div>
   );
 }
