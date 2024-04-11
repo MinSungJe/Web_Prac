f8ac162342eed924adc35800083b89a4c24ca962
Minje Sung <minje813@dgu.ac.kr>
2024-04-11 16:33:37 +0900

    20240411

--- JavaScript/tab.js
+++ JavaScript/tab.js
@@ -1,8 +1,10 @@
-[0, 1, 2].map((i) => {
-    $('.tab-button').eq(i).on('click', () => {
-        $('.tab-button').removeClass('orange')
-        $('.tab-button').eq(i).addClass('orange')
+var button = $('.tab-button')
+
+for (let i = 0; i < button.length; i++) {
+    button.eq(i).on('click', () => {
+        button.removeClass('orange')
+        button.eq(i).addClass('orange')
         $('.tab-content').removeClass('show')
         $('.tab-content').eq(i).addClass('show')
     })
-})
+}
